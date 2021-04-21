79106591496e20ff196ca05098d72d6a7fe140d7
Zdravko Zdravkov <zdr0@yahoo.com>
2021-04-21 16:44:02 +0200

    A_UsingApiControllerAttribute

---
+++ 02. ASP.NET Core In Action/Chapter09/A_UsingApiControllerAttribute/UsingApiControllerAttribute/Controllers/FruitController.cs
@@ -0,0 +1,34 @@
+﻿namespace UsingApiControllerAttribute.Controllers
+{
+    using System.Collections.Generic;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    [ApiController]
+    [Route("[controller]")]
+    public class FruitController : ControllerBase
+    {
+        private readonly List<string> _fruit = new()
+        {
+            "Pear", "Lemon", "Peach"
+        };
+
+        [HttpGet]
+        public ActionResult Update()
+        {
+            return this.Ok(this._fruit);
+        }
+
+        [HttpPost]
+        public ActionResult Update(UpdateModel model)
+        {
+            if (model.Id < 0 || model.Id > this._fruit.Count)
+            {
+                return this.NotFound();
+            }
+
+            this._fruit[model.Id] = model.Name;
+            return this.Ok();
+        }
+    }
+}
---
+++ 02. ASP.NET Core In Action/Chapter09/A_UsingApiControllerAttribute/UsingApiControllerAttribute/Controllers/NoAttributeController.cs
@@ -0,0 +1,44 @@
+﻿namespace UsingApiControllerAttribute.Controllers
+{
+    using System.Collections.Generic;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("[controller]")]
+    public class NoAttributeController : ControllerBase
+    {
+        private readonly List<string> _fruit = new()
+        {
+            "Pear", "Lemon", "Peach"
+        };
+
+        [HttpGet]
+        public ActionResult Update()
+        {
+            return this.Ok(this._fruit);
+        }
+
+        [HttpPost]
+        public ActionResult Update([FromBody] UpdateModel model)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return this.BadRequest(new ValidationProblemDetails(this.ModelState));
+            }
+
+            if (model.Id < 0 || model.Id > this._fruit.Count)
+            {
+                return this.NotFound(new ProblemDetails()
+                {
+                    Status = 404,
+                    Title = "Not Found",
+                    Type = "https://tools.ietf.org/html/rfc7231#section-6.5.4",
+                });
+            }
+
+            this._fruit[model.Id] = model.Name;
+
+            return this.Ok();
+        }
+    }
+}
