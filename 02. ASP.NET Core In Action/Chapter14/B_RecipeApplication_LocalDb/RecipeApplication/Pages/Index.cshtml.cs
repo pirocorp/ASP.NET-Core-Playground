9eb789ccf4b899cebccd204c14df590c13028f1b
Zdravko Zdravkov <zdr0@yahoo.com>
2021-05-04 16:13:30 +0200

    Chapter 14

---
+++ 02. ASP.NET Core In Action/Chapter14/B_RecipeApplication_LocalDb/RecipeApplication/Pages/Index.cshtml.cs
@@ -0,0 +1,26 @@
+﻿namespace RecipeApplication.Pages
+{
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+
+    using RecipeApplication.Models;
+
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    public class IndexModel : PageModel
+    {
+        private readonly RecipeService _service;
+        public IEnumerable<RecipeSummaryViewModel> Recipes { get; private set; }
+
+        public IndexModel(RecipeService service)
+        {
+            this._service = service;
+        }
+
+        public async Task OnGet()
+        {
+            this.Recipes = await this._service.GetRecipes();
+
+        }
+    }
+}
---
+++ 02. ASP.NET Core In Action/Chapter14/C_RecipeApplicationWithNameClaim_LocalDb/RecipeApplication/Pages/Recipes/View.cshtml.cs
@@ -0,0 +1,38 @@
+﻿namespace RecipeApplication.Pages.Recipes
+{
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+
+    using RecipeApplication.Models;
+
+    public class ViewModel : PageModel
+    {
+        public RecipeDetailViewModel Recipe { get; set; }
+        private readonly RecipeService _service;
+        public ViewModel(RecipeService service)
+        {
+            this._service = service;
+        }
+
+        public async Task<IActionResult> OnGetAsync(int id)
+        {
+            this.Recipe = await this._service.GetRecipeDetail(id);
+            if (this.Recipe is null)
+            {
+                // If id is not for a valid Recipe, generate a 404 error page
+                // TODO: Add status code pages middleware to show friendly 404 page
+                return this.NotFound();
+            }
+            return this.Page();
+        }
+
+        public async Task<IActionResult> OnPostDeleteAsync(int id)
+        {
+            await this._service.DeleteRecipe(id);
+
+            return this.RedirectToPage("/Index");
+        }
+    }
+}
