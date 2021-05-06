fc2448d9f32451b05d4a76243ca35d13e2eaaee0
Zdravko Zdravkov <zdr0@yahoo.com>
2021-05-06 18:32:42 +0200

    Chapter 18

---
+++ 02. ASP.NET Core In Action/Chapter18/B_CrossSiteScripting/CrossSiteScripting/Pages/Protected.cshtml.cs
@@ -0,0 +1,27 @@
+﻿namespace CrossSiteScripting.Pages
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+
+    public class ProtectedModel : PageModel
+    {
+        [BindProperty]
+        public string Name { get; set; }
+
+        public void OnGet()
+        {
+            this.Name = DataService.GetMaliciousValue();
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return this.Page();
+            }
+
+            DataService.Data.Add(this.Name);
+            return this.RedirectToPage("/protected");
+        }
+    }
+}
---
+++ 02. ASP.NET Core In Action/Chapter18/C_CrossSiteRequestForgery/BankingApplication/Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+﻿namespace BankingApplication.Pages
+{
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+    using Microsoft.Extensions.Logging;
+
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            this._logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
